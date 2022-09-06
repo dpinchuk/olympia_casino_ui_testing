b1429ea203251cfa5fc88a36857f0da484a4555f
Dmitry Pinchuk <pinchuk.dap@gmail.com>
2022-09-06 14:10:39 +0300

    Added wrap sign up method

--- cypress/integration/signUp/signUp.spec.js
+++ cypress/integration/signUp/signUp.spec.js
@@ -1,29 +1,11 @@
+import '../../support/common/signUp.support';
 import {homeSteps} from '../../page-steps/home/home.steps'
-import {signUpSteps} from '../../page-steps/signUp/signUp.steps'
 
 describe('Sign up a new user account', () => {
     it('should sign up a new user account', () => {
         homeSteps.openHomePage();
         homeSteps.clickSignUpButton();
-        signUpSteps.enterEmail();
-        signUpSteps.enterPassword();
-        signUpSteps.selectCurrency('AUD');
-        signUpSteps.clickNext();
-        signUpSteps.enterFirstName();
-        signUpSteps.enterLastName();
-        signUpSteps.enterBirthDay();
-        signUpSteps.enterBirthMonth();
-        signUpSteps.enterBirthYear();
-        signUpSteps.selectMobilePhoneCode('+1');
-        signUpSteps.enterMobilePhoneNumber();
-        signUpSteps.clickNext();
-        signUpSteps.selectCountry('Turkey')
-        signUpSteps.enterCity();
-        signUpSteps.enterAddress();
-        signUpSteps.enterPostalCode();
-        signUpSteps.clickReceivePromos(true);
-        signUpSteps.clickReceiveSmsPromos(true);
-        signUpSteps.clickTermsAcceptance(true);
-        //SIGN_UP...
+        cy.signUp('AUD', '+1', 'Turkey', true, true, true);
+        //Click SIGN_UP...
     });
 })
---
+++ cypress/support/common/signUp.support.js
@@ -0,0 +1,24 @@
+import '../commands';
+import {signUpSteps} from "../../page-steps/signUp/signUp.steps";
+
+Cypress.Commands.add('signUp', (currency, mobilePhoneCode, country, isReceivePromos, isReceiveSmsPromos, isTermsAcceptance) => {
+    signUpSteps.enterEmail();
+    signUpSteps.enterPassword();
+    signUpSteps.selectCurrency(currency);
+    signUpSteps.clickNext();
+    signUpSteps.enterFirstName();
+    signUpSteps.enterLastName();
+    signUpSteps.enterBirthDay();
+    signUpSteps.enterBirthMonth();
+    signUpSteps.enterBirthYear();
+    signUpSteps.selectMobilePhoneCode(mobilePhoneCode);
+    signUpSteps.enterMobilePhoneNumber();
+    signUpSteps.clickNext();
+    signUpSteps.selectCountry(country);
+    signUpSteps.enterCity();
+    signUpSteps.enterAddress();
+    signUpSteps.enterPostalCode();
+    signUpSteps.clickReceivePromos(isReceivePromos);
+    signUpSteps.clickReceiveSmsPromos(isReceiveSmsPromos);
+    signUpSteps.clickTermsAcceptance(isTermsAcceptance);
+})
